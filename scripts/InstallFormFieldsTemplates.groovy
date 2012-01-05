6a5aa0b0edbb7480310174b47cc7d1e9e22f060e
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-01-05 12:29:04 +0000

    added licence & removed cruft

--- scripts/InstallFormFieldsTemplates.groovy
+++ scripts/InstallFormFieldsTemplates.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Rob Fletcher
+ * Copyright 2012 Rob Fletcher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

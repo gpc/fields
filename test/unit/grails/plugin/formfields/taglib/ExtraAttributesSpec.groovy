e3db6463e163bee814c20d326f9f3df350476e46
bluesliverx <bksaville@gmail.com>
2012-03-14 21:10:25 -0600

    #49 added unit tests for extra attributes on input and field

--- test/unit/grails/plugin/formfields/taglib/ExtraAttributesSpec.groovy
+++ test/unit/grails/plugin/formfields/taglib/ExtraAttributesSpec.groovy
@@ -70,4 +70,35 @@ class ExtraAttributesSpec extends AbstractFormFieldsTagLibSpec {
 		applyTemplate('<f:input bean="personInstance" property="name" foo="bar"/>', [personInstance: personInstance]) == '<input type="text" foo="bar" name="name" value="Bart Simpson" required="" id="name" />'
 	}
 
+	@Issue("https://github.com/robfletcher/grails-fields/pull/49")
+    void 'arbitrary attributes prefixed with input- are not passed to the field template'() {
+        given:
+        views["/_fields/default/_field.gsp"] = '<foo>${foo}</foo>'
+
+        expect:
+        applyTemplate('<f:field bean="personInstance" property="name" input-foo="bar" />', [personInstance: personInstance]) == '<foo></foo>'
+    }
+
+	@Issue("https://github.com/robfletcher/grails-fields/pull/49")
+    void 'arbitrary attributes prefixed with input- on f:field are passed to the input template'() {
+        given:
+        views["/_fields/default/_field.gsp"] = '${widget}'
+        views["/_fields/person/name/_input.gsp"] = '<span>${foo}</span>'
+
+		and:
+		mockFormFieldsTemplateService.findTemplate(_, 'input') >> [path: '/_fields/person/name/input']
+
+        expect:
+        applyTemplate('<f:field bean="personInstance" property="name" input-foo="bar"/>', [personInstance: personInstance]) == '<span>bar</span>'
+    }
+
+	@Issue("https://github.com/robfletcher/grails-fields/pull/49")
+    void 'arbitrary attributes prefixed with input- on f:field are passed to the default input'() {
+        given:
+        views["/_fields/default/_field.gsp"] = '${widget}'
+
+        expect:
+        applyTemplate('<f:field bean="personInstance" property="name" input-foo="bar"/>', [personInstance: personInstance]) == '<input type="text" foo="bar" name="name" value="Bart Simpson" required="" id="name" />'
+    }
+
 }

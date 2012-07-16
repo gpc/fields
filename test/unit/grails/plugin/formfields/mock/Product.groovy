5342909f74df06a4e5a3005dcefbf8e682270b38
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-07-16 17:04:03 +0100

    failing test for #85 and #87
    doesn't seem to replicate the issue correctly using @Mock

---
+++ test/unit/grails/plugin/formfields/mock/Product.groovy
@@ -0,0 +1,31 @@
+package grails.plugin.formfields.mock
+
+import grails.persistence.Entity
+
+@Entity
+class Product {
+
+    String name
+    Double netPrice
+    Double taxRate
+    Double tax
+    Double grossPrice
+
+    static transients = ['grossPrice']
+
+    static constraints = {
+        name blank: false, unique: true
+        netPrice min: 0.01d
+        taxRate min: 0.0d
+        tax nullable: true
+    }
+
+    static mapping = {
+        tax formula: 'NET_PRICE * TAX_RATE'
+    }
+
+    def onLoad() {
+        grossPrice = netPrice + tax
+    }
+
+}

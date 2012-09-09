5617ff7cc56308c542fe80d5279cea2576e7d4a2
Patrick Hackbarth <patrick.hackbarth@gmx.de>
2012-09-09 02:17:18 +0200

    Added: template location lookup control switches in order to reduce "groovyPageLocator.findTemplateByPath(path)" calls.

--- grails-app/services/grails/plugin/formfields/FormFieldsTemplateService.groovy
+++ grails-app/services/grails/plugin/formfields/FormFieldsTemplateService.groovy
@@ -30,6 +30,14 @@ class FormFieldsTemplateService {
     GrailsConventionGroovyPageLocator groovyPageLocator
     GrailsPluginManager pluginManager
 
+    boolean ignoreControllerLookup = false
+    boolean ignoreControllerActionLookup = false
+    boolean ignoreAssociationLookup = false
+    boolean ignoreBeanTypeLookup = false
+    boolean ignoreBeanSuperTypeLookup = false
+    boolean ignorePropertyTypeLookup = false
+    boolean ignorePropertySuperTypeLookup = false
+
     Map findTemplate(BeanPropertyAccessor propertyAccessor, String templateName) {
         findTemplateCached(propertyAccessor, controllerName, actionName, templateName)
     }
@@ -64,44 +72,71 @@ class FormFieldsTemplateService {
         def templateResolveOrder = []
 
         // if there is a controller for the current request any template in its views directory takes priority
-        if (controllerName) {
+        if (!ignoreControllerLookup && controllerName) {
             // first try action-specific templates
-            templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, propertyAccessor.propertyName, templateName)
-            if (propertyAccessor.propertyType) templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, toPropertyNameFormat(propertyAccessor.propertyType), templateName)
-            templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, templateName)
+            if (!ignoreControllerActionLookup) appendControllerActionTemplates(templateResolveOrder, controllerName, actionName, propertyAccessor, templateName)
 
             // then general templates for the controller
-            templateResolveOrder << appendPiecesForUri("/", controllerName, propertyAccessor.propertyName, templateName)
-            if (propertyAccessor.propertyType) templateResolveOrder << appendPiecesForUri("/", controllerName, toPropertyNameFormat(propertyAccessor.propertyType), templateName)
-            templateResolveOrder << appendPiecesForUri("/", controllerName, templateName)
+            appendControllerTemplates(templateResolveOrder, controllerName, propertyAccessor, templateName)
         }
 
         // if we have a bean type look in `grails-app/views/_fields/<beanType>/<propertyName>/_field.gsp` and equivalent for superclasses
-        if (propertyAccessor.beanType) {
-            templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertyAccessor.beanType), propertyAccessor.propertyName, templateName)
-            for (superclass in propertyAccessor.beanSuperclasses) {
-                templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(superclass), propertyAccessor.propertyName, templateName)
-            }
+        if (!ignoreBeanTypeLookup && propertyAccessor.beanType) {
+            appendBeanTypeTemplates(templateResolveOrder, propertyAccessor, templateName)
+            if (!ignoreBeanSuperTypeLookup) appendBeanSuperTypeTemplates(templateResolveOrder, propertyAccessor, templateName)
         }
 
         // if this is an association property look in `grails-app/views/_fields/<associationType>/_field.gsp`
-        def associationPath = getAssociationPath(propertyAccessor)
-        if (associationPath) {
-            templateResolveOrder << appendPiecesForUri('/_fields', associationPath, templateName)
-        }
-
+        if (!ignoreAssociationLookup) appendAssociationTemplates(templateResolveOrder, propertyAccessor, templateName)
         // if we have a property type look in `grails-app/views/_fields/<propertyType>/_field.gsp` and equivalent for superclasses
-        if (propertyAccessor.propertyType) {
-            templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertyAccessor.propertyType), templateName)
-            for (propertySuperClass in propertyAccessor.propertyTypeSuperclasses) {
-                templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertySuperClass), templateName)
-            }
+        if (!ignorePropertyTypeLookup && propertyAccessor.propertyType) {
+            appendPropertyTypeTemplates(templateResolveOrder, propertyAccessor, templateName)
+            if (!ignorePropertySuperTypeLookup) appendPropertySuperTypeTemplates(templateResolveOrder, propertyAccessor, templateName)
         }
 
         // if nothing else is found fall back to a default (even this may not exist for f:input)
         templateResolveOrder << "/_fields/default/$templateName"
 
         templateResolveOrder
+    }
+
+    protected void appendControllerTemplates(ArrayList templateResolveOrder, String controllerName, BeanPropertyAccessor propertyAccessor, String templateName) {
+        templateResolveOrder << appendPiecesForUri("/", controllerName, propertyAccessor.propertyName, templateName)
+        if (propertyAccessor.propertyType) templateResolveOrder << appendPiecesForUri("/", controllerName, toPropertyNameFormat(propertyAccessor.propertyType), templateName)
+        templateResolveOrder << appendPiecesForUri("/", controllerName, templateName)
+    }
+
+    protected void appendControllerActionTemplates(ArrayList templateResolveOrder, String controllerName, String actionName, BeanPropertyAccessor propertyAccessor, String templateName) {
+        templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, propertyAccessor.propertyName, templateName)
+        if (propertyAccessor.propertyType) templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, toPropertyNameFormat(propertyAccessor.propertyType), templateName)
+        templateResolveOrder << appendPiecesForUri("/", controllerName, actionName, templateName)
+    }
+
+    protected void appendBeanTypeTemplates(ArrayList templateResolveOrder, BeanPropertyAccessor propertyAccessor, String templateName) {
+        templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertyAccessor.beanType), propertyAccessor.propertyName, templateName)
+    }
+
+    protected void appendBeanSuperTypeTemplates(ArrayList templateResolveOrder, BeanPropertyAccessor propertyAccessor, String templateName) {
+        for (superclass in propertyAccessor.beanSuperclasses) {
+            templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(superclass), propertyAccessor.propertyName, templateName)
+        }
+    }
+
+    protected void appendAssociationTemplates(ArrayList templateResolveOrder, BeanPropertyAccessor propertyAccessor, String templateName) {
+        def associationPath = getAssociationPath(propertyAccessor)
+        if (associationPath) {
+            templateResolveOrder << appendPiecesForUri('/_fields', associationPath, templateName)
+        }
+    }
+
+    protected void appendPropertyTypeTemplates(ArrayList templateResolveOrder, BeanPropertyAccessor propertyAccessor, String templateName) {
+        templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertyAccessor.propertyType), templateName)
+    }
+
+    protected void appendPropertySuperTypeTemplates(ArrayList templateResolveOrder, BeanPropertyAccessor propertyAccessor, String templateName) {
+        for (propertySuperClass in propertyAccessor.propertyTypeSuperclasses) {
+            templateResolveOrder << appendPiecesForUri("/_fields", toPropertyNameFormat(propertySuperClass), templateName)
+        }
     }
 
     private String getAssociationPath(BeanPropertyAccessor propertyAccessor) {

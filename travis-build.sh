fc8b1fa71e63570ba01d78e2000e646fd7ee206d
Graeme Rocher <graemerocher@Graemes-iMac.local>
2015-01-27 10:48:16 +0100

    add automatic doc publishing and Bintray releases

--- travis-build.sh
+++ travis-build.sh
@@ -7,27 +7,59 @@ filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
-  ./gradlew install publish 
 
-  #./gradlew docs
-  #
-  #git config --global user.name "$GIT_NAME"
-  #git config --global user.email "$GIT_EMAIL"
-  #git config --global credential.helper "store --file=~/.git-credentials"
-  #echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+    echo "Publishing archives"
 
-  #if [[ $filename != *-SNAPSHOT* ]]
-  #then
-  #  git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages gh-pages --single-branch > /dev/null
-  #  cd gh-pages
-  #  git rm -rf .
-  #  cp -r ../build/docs/manual/. ./
-  #  git add *
-  #  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-  #  git push origin HEAD
-  #  cd ..
-  #  rm -rf gh-pages
-  #else
-  #  echo "SNAPSHOT version, not publishing docs"
-  #fi
+    if [[ -n $TRAVIS_TAG ]]; then
+        ./gradlew bintrayUpload || EXIT_STATUS=$?
+    else
+        ./gradlew publish || EXIT_STATUS=$?
+    fi
+
+    ./gradlew docs || EXIT_STATUS=$?
+
+    git config --global user.name "$GIT_NAME"
+    git config --global user.email "$GIT_EMAIL"
+    git config --global credential.helper "store --file=~/.git-credentials"
+    echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
+  git clone https://${GH_TOKEN}@github.com/grails3-plugins/fields.git -b gh-pages gh-pages --single-branch > /dev/null
+  cd gh-pages
+
+  # If this is the master branch then update the snapshot
+  if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    mkdir -p snapshot
+    cp -r ../build/docs/manual/. ./snapshot/
+
+    git add snapshot/*
+  fi
+
+    # If there is a tag present then this becomes the latest
+    if [[ -n $TRAVIS_TAG ]]; then
+        if [[ $TRAVIS_BRANCH == 'master' ]]; then
+            git rm -rf latest/
+            mkdir -p latest
+            cp -r ../build/docs/manual/. ./latest/
+            git add latest/*
+        fi
+
+        version="$TRAVIS_TAG"
+        version=${version:1}
+        majorVersion=${version:0:4}
+        majorVersion="${majorVersion}x"
+
+        mkdir -p "$version"
+        cp -r ../build/docs/manual/. "./$version/"
+        git add "$version/*"
+
+        mkdir -p "$majorVersion"
+        cp -r ../build/docs/manual/. "./$majorVersion/"
+        git add "$majorVersion/*"
+
+    fi
+
+    git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+    git push origin HEAD
+    cd ..
+    rm -rf gh-pages
 fi

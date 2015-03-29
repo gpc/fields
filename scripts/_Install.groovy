a9e592f0148057f877ea5fc7ea158add8d8c01d1
Martín Caballero <mpccolorado@gmail.com>
2015-03-29 15:31:21 -0300

    Install script was added just like domurtag proposed.

---
+++ scripts/_Install.groovy
@@ -0,0 +1,4 @@
+import grails.build.logging.GrailsConsole
+
+GrailsConsole.instance.warn '''\
+This release contains breaking changes. If upgrading from a previous version, please see 'Breaking changes' section in http://grails-fields-plugin.github.io/grails-fields/guide/usage.html'''

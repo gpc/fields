b0f694f1526a192037b317b6edf70b52a66d09c7
domurtag <domurtag@users.noreply.github.com>
2015-04-26 22:43:42 +0100

    changed version number before release

--- FieldsGrailsPlugin.groovy
+++ FieldsGrailsPlugin.groovy
@@ -19,7 +19,7 @@ import org.codehaus.groovy.grails.validation.ConstraintsEvaluator
 
 class FieldsGrailsPlugin {
 
-	def version = '1.5-SNAPSHOT'
+	def version = '1.5'
 	def grailsVersion = '2.0 > *'
 	def dependsOn = [:]
 	def pluginExcludes = []

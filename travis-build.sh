93b07b5ca348be5bb9fec9a0f40c66a1898adf2c
Tom Crossland <tom.crossland@bluetab.net>
2016-02-04 13:12:19 +0100

    Fix travis build script (use sdkman instead of GVM)

--- travis-build.sh
+++ travis-build.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
-function load_gvm {
-	if [ ! -f ~/.gvm/etc/config ]; then
-		# ~/.gvm is some golang related tool in Travis CI, just remove it and install gvm
-		rm -rf ~/.gvm
-		curl -s get.gvmtool.net | bash
-		perl -i -p -e 's/gvm_auto_answer=false/gvm_auto_answer=true/' ~/.gvm/etc/config
+function load_sdkman {
+	if [ ! -e ~/.sdkman ]; then
+		curl -s http://get.sdkman.io | bash
+		perl -i -p -e 's/sdkman_auto_answer=false/sdkman_auto_answer=false/' ~/.sdkman/etc/config
 	fi
 
-	source ~/.gvm/bin/gvm-init.sh
+	export SDKMAN_DIR=~/.sdkman && source ~/.sdkman/bin/sdkman-init.sh
 }
 
 function install_and_use_grails {
 	grails_version=$1
-	load_gvm
-	gvm install grails $grails_version
-	if [ $? -ne 0 ]; then
-		# grails version not available in gvm yet, download directly from s3
-		set -e
-		(
-		set -e
-		cd /tmp
-		curl -O http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-${grails_version}.zip
-		unzip grails-${grails_version}.zip -d ~/.gvm/grails/ 
-		mv ~/.gvm/grails/{grails-${grails_version},${grails_version}}
-		rm grails-${grails_version}.zip
-		)	
-	fi
-	gvm default grails $grails_version
-	gvm use grails $grails_version
+	load_sdkman
+	sdk install grails $grails_version
+	sdk default grails $grails_version
+	sdk use grails $grails_version
 }
 
 use_grails_version="${GRAILS_VERSION:-2.4.4}"
 install_and_use_grails $use_grails_version
 perl -i -p -e "s/app\\.grails\\.version=.*/app.grails.version=$use_grails_version/" application.properties
-
 
 set -e
 grails refresh-dependencies --non-interactive

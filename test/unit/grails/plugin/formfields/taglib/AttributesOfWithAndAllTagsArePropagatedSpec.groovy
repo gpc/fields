808b37ab3fd1195462aee454e0b3726185293097
Martín <mpccolorado@gmail.com>
2016-04-07 20:50:47 -0300

    Test improved for issue #210

--- test/unit/grails/plugin/formfields/taglib/AttributesOfWithAndAllTagsArePropagatedSpec.groovy
+++ test/unit/grails/plugin/formfields/taglib/AttributesOfWithAndAllTagsArePropagatedSpec.groovy
@@ -2,8 +2,7 @@ package grails.plugin.formfields.taglib
 
 import grails.plugin.formfields.FormFieldsTagLib
 import grails.plugin.formfields.FormFieldsTemplateService
-import grails.plugin.formfields.mock.Employee
-import grails.plugin.formfields.mock.Person
+import grails.plugin.formfields.mock.Product
 import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Ignore
@@ -11,7 +10,7 @@ import spock.lang.Issue
 import spock.lang.Unroll
 
 @TestFor(FormFieldsTagLib)
-@Mock([Person, Employee])
+@Mock([Product])
 @Unroll
 @Issue('https://github.com/grails-fields-plugin/grails-fields/issues/210')
 class AttributesOfWithAndAllTagsArePropagatedSpec extends AbstractFormFieldsTagLibSpec {
@@ -26,6 +25,10 @@ class AttributesOfWithAndAllTagsArePropagatedSpec extends AbstractFormFieldsTagL
 		def taglib = applicationContext.getBean(FormFieldsTagLib)
 
 		mockFormFieldsTemplateService.getWidgetPrefix() >> "widget-"
+		mockFormFieldsTemplateService.getTemplateFor("wrapper") >> 'wrapper'
+		mockFormFieldsTemplateService.getTemplateFor("displayWrapper") >> 'displayWrapper'
+		mockFormFieldsTemplateService.getTemplateFor("widget") >> 'widget'
+		mockFormFieldsTemplateService.getTemplateFor("displayWidget") >> 'displayWidget'
 
 		mockFormFieldsTemplateService.findTemplate(_, 'wrapper', null) >> [path: '/_fields/default/wrapper']
 		mockFormFieldsTemplateService.findTemplate(_, 'displayWrapper', null) >> [path: '/_fields/default/displayWrapper']
@@ -33,111 +36,98 @@ class AttributesOfWithAndAllTagsArePropagatedSpec extends AbstractFormFieldsTagL
 		mockFormFieldsTemplateService.findTemplate(_, 'widget', null) >> [path: '/_fields/default/widget']
 		mockFormFieldsTemplateService.findTemplate(_, 'displayWidget', null) >> [path: '/_fields/default/displayWidget']
 
-		mockFormFieldsTemplateService.findTemplate(_, 'wrapper', "cool") >> [path: '/_fields/cool/wrapper']
-		mockFormFieldsTemplateService.findTemplate(_, 'displayWrapper', "cool") >> [path: '/_fields/cool/displayWrapper']
-
-		mockFormFieldsTemplateService.getTemplateFor('wrapper') >> "wrapper"
-		mockFormFieldsTemplateService.getTemplateFor('widget') >> "widget"
-		mockFormFieldsTemplateService.getTemplateFor('displayWrapper') >> "displayWrapper"
-		mockFormFieldsTemplateService.getTemplateFor('displayWidget') >> "displayWidget"
-
 		taglib.formFieldsTemplateService = mockFormFieldsTemplateService
 
 		mockEmbeddedSitemeshLayout(taglib)
 
-		views["/_fields/_embedded.gsp"] = '<g:layoutBody/>'
-		views["/_fields/default/_wrapper.gsp"] = '<wrapper>${widget}</wrapper>'
-		views["/_fields/default/_displayWrapper.gsp"] = '<displayWrapper>${widget}</displayWrapper>'
-		views["/_fields/default/_widget.gsp"] = 'widget'
-		views["/_fields/default/_displayWidget.gsp"] = 'displayWidget'
-		views["/_fields/cool/_wrapper.gsp"] = '<coolWrapper>${widget}</coolWrapper>'
-		views["/_fields/cool/_displayWrapper.gsp"] = '<coolDisplayWrapper>${widget}</coolDisplayWrapper>'
-		views["/_fields/cool/_widget.gsp"] = 'coolWidget'
-		views["/_fields/cool/_displayWidget.gsp"] = 'coolDisplayWidget'
+		views["/_fields/default/_wrapper.gsp"] = '<wrapper attr="${attribute}">${widget}</wrapper>'
+		views["/_fields/default/_displayWrapper.gsp"] = '<displayWrapper attr="${attribute}">${widget}</displayWrapper>'
+		views["/_fields/default/_widget.gsp"] = '<widget>${property}</widget>'
+		views["/_fields/default/_displayWidget.gsp"] = '<displayWidget>${property}</displayWidget>'
 	}
 
 	void "An attribute of the <f:with> tag is present on a field tag"() {
 		expect:
 		applyTemplate(
-				'<f:with bean="personInstance" wrapper="cool">' +
+				'<f:with bean="productInstance" attribute="cool">' +
 					'<f:field property="name"/>' +
 				'</f:with>',
-				[personInstance: personInstance]
-		) == "<coolWrapper>widget</coolWrapper>"
+				[productInstance: productInstance]
+		) == '<wrapper attr="cool"><widget>name</widget></wrapper>'
 	}
 
 	void "An attribute of the <f:with> tag is present on an inner display tag"() {
 		expect:
 		applyTemplate(
-				'<f:with bean="personInstance" wrapper="cool">' +
+				'<f:with bean="productInstance" attribute="cool">' +
 					'<f:display property="name"/>' +
 				'</f:with>',
-				[personInstance: personInstance]
-		) == "<coolDisplayWrapper>displayWidget</coolDisplayWrapper>"
+				[productInstance: productInstance]
+		) == '<displayWrapper attr="cool"><displayWidget>name</displayWidget></displayWrapper>'
 	}
 
 	void "An attribute of the <f:all> tag is present on inner fields tag"() {
 		expect:
 		applyTemplate(
-				'<f:all bean="personInstance" wrapper="cool"/>',
-				[personInstance: personInstance]
-		) == "<coolWrapper>widget</coolWrapper>" * 15 //15 is the number of properties of the person class
+				'<f:all bean="productInstance" attribute="cool"/>',
+				[productInstance: productInstance]
+		) == '<wrapper attr="cool"><widget>name</widget></wrapper>' +
+			 '<wrapper attr="cool"><widget>netPrice</widget></wrapper>' +
+			 '<wrapper attr="cool"><widget>taxRate</widget></wrapper>' +
+			 '<wrapper attr="cool"><widget>tax</widget></wrapper>'
 	}
 
 	@Ignore("It is not possible to use the <f:all> tag for printing displays instead of fields")
 	void "An attribute of the <f:displayAll> tag is present on inner displays tag"() {
 		expect:
 		applyTemplate(
-				'<f:displayAll bean="personInstance" wrapper="cool"/>',
-				[personInstance: personInstance]
-		) == "<coolWrapper>displayWidget</coolWrapper>" * 15 //15 is the number of properties of the person class
+				'<f:displayAll bean="productInstance" wrapper="cool"/>',
+				[productInstance: productInstance]
+		) == '<displayWrapper attr="cool"><displayWidget>name</displayWidget></displayWrapper>' +
+			 '<displayWrapper attr="cool"><displayWidget>netPrice</displayWidget></displayWrapper>' +
+			 '<displayWrapper attr="cool"><displayWidget>taxRate</displayWidget></displayWrapper>' +
+			 '<displayWrapper attr="cool"><displayWidget>tax</displayWidget></displayWrapper>'
 	}
 
 	//Test Override of attributes
 
 	void "<f:field> attributes overrides <f:with> attributes"() {
-		given:
-		views["/_fields/default/_widget.gsp"] = '<div class="${someClass}">widget</div>'
-
 		expect:
 		applyTemplate(
-				'<f:with bean="personInstance" widget-someClass="general-class">' +
-					'<f:field property="name" widget-someClass="override-class"/>' +
+				'<f:with bean="productInstance" attribute="general-attr">' +
+					'<f:field property="name" attribute="override-attr"/>' +
 				'</f:with>',
-				[personInstance: personInstance]
-		) == '<wrapper><div class="override-class">widget</div></wrapper>'
+				[productInstance: productInstance]
+		) == '<wrapper attr="override-attr"><widget>name</widget></wrapper>'
 	}
 
 	void "<f:display> attributes overrides <f:with> attributes"() {
-		given:
-		views["/_fields/default/_displayWidget.gsp"] = '<div class="${someClass}">displayWidget</div>'
-
 		expect:
 		applyTemplate(
-				'<f:with bean="personInstance" widget-someClass="general-class">' +
-						'<f:display property="name" widget-someClass="override-class"/>' +
+				'<f:with bean="productInstance" attribute="general-attr">' +
+						'<f:display property="name" attribute="override-attr"/>' +
 				'</f:with>',
-				[personInstance: personInstance]
-		) == '<displayWrapper><div class="override-class">displayWidget</div></displayWrapper>'
+				[productInstance: productInstance]
+		) == '<displayWrapper attr="override-attr"><displayWidget>name</displayWidget></displayWrapper>'
 	}
 
 	//Test cascade tags
 
 	void "Test cascade of <f:with> tags"() {
-		given:
-		views["/_fields/default/_widget.gsp"] = '<div class="${someClass}">${property}</div>'
-
 		expect:
 		applyTemplate(
-				'<f:with bean="personInstance" wrapper="cool" widget-someClass="defaultWidgetClass">' +
+				'<f:with bean="productInstance" attribute="general-attr">' +
 					'<f:field property="name"/>' +
-					'<f:with bean="personInstance" wrapper="default">' +
-						'<f:field property="password"/>' +
+					'<f:field property="netPrice"/>' +
+					'<f:with bean="productInstance" attribute="override-attr">' +
+						'<f:field property="taxRate"/>' +
 					'</f:with>' +
+					'<f:display property="tax"/>' +
 				'</f:with>',
-				[personInstance: personInstance]
-		) == '<coolWrapper><div class="defaultWidgetClass">name</div></coolWrapper>' +
-			 '<wrapper><div class="defaultWidgetClass">password</div></wrapper>'
+				[productInstance: productInstance]
+		) == '<wrapper attr="general-attr"><widget>name</widget></wrapper>' +
+			 '<wrapper attr="general-attr"><widget>netPrice</widget></wrapper>' +
+			 '<wrapper attr="override-attr"><widget>taxRate</widget></wrapper>' +
+			 '<displayWrapper attr="general-attr"><displayWidget>tax</displayWidget></displayWrapper>'
 	}
-
 }

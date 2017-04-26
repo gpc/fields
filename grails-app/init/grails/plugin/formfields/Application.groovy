7b973eb330265de6f8e655fe7a6bab67235445f8
graemerocher <graeme.rocher@gmail.com>
2017-04-26 12:37:06 +0200

    Convert docs to asciidoc

---
+++ grails-app/init/grails/plugin/formfields/Application.groovy
@@ -0,0 +1,11 @@
+package grails.plugin.formfields
+
+import grails.boot.GrailsApp
+import grails.boot.config.GrailsAutoConfiguration
+
+class Application extends GrailsAutoConfiguration  {
+
+    static void main(String[] args) {
+        GrailsApp.run(Application, args)
+    }
+}

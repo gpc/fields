2e1191ded1500d53ff69dbe2136497559153b27c
sdelamo <sergio.delamo@softamo.com>
2018-12-06 10:07:23 +0100

    Open JDK 11 in travis

--- travis-build.sh
+++ travis-build.sh
@@ -2,13 +2,32 @@
 set -e
 chmod +x ./travis/*.sh
 
-./gradlew test assemble --no-daemon
+EXIT_STATUS=0
+
+echo "Test for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
+
+./gradlew test --no-daemon || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+echo "Assemble for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
+
+./gradlew assemble --no-daemon || EXIT_STATUS=$?
+
+if [ $EXIT_STATUS -ne 0 ]; then
+  exit $EXIT_STATUS
+fi
+
+if [ "${TRAVIS_JDK_VERSION}" == "openjdk11" ] ; then
+  exit $EXIT_STATUS
+fi
 
 filename=$(find build/libs -name "*.jar" | head -1)
 filename=$(basename "$filename")
 
-EXIT_STATUS=0
-echo "Publishing archives for branch $TRAVIS_BRANCH"
+echo "Publishing archives for branch $TRAVIS_BRANCH JDK: $TRAVIS_JDK_VERSION"
 if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_PULL_REQUEST == 'false' ]]; then
 
   echo "Publishing archives"

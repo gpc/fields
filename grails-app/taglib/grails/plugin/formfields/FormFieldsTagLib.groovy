038e355e259cbb72e123a4c60c6de6e93c0e7ba0
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-09-13 00:35:21 -0700

    Document new classes

--- grails-app/taglib/grails/plugin/formfields/FormFieldsTagLib.groovy
+++ grails-app/taglib/grails/plugin/formfields/FormFieldsTagLib.groovy
@@ -168,6 +168,9 @@ class FormFieldsTagLib {
 	 * @attr widget Specify the folder inside _fields where to look up for the widget template.
 	 * @attr templates Specify the folder inside _fields where to look up for the wrapper and widget template.
 	 * @attr theme Theme name
+	 * @attr divClass class for optional div that will be added if set and contain the widget
+	 * @attr invalidClass class added to wrapper that if property is invalid. Also available for widget.
+	 * @attr requiredClass class added to wrapper when property is required. Also available for widget.
 	 */
 	def field = { attrs, body ->
 		attrs = beanStack.innerAttributes + attrs
